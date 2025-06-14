19f4a333b6a7a8c081a937544b5ba3de595c8a8d
Rushi Kulkarni <ruku508501@gmail.com>
2025-06-14 13:41:03 -0400

    chore: remove unused report cleanup code from e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -7,12 +7,3 @@ Cypress.on('uncaught:exception', (err, runnable) =>{
     return false
 })
 
-const reportsDir = path.resolve('cypress/reports');
-
-before(() => {
-    if (fs.existsSync(reportsDir)) {
-        fs.rmSync(reportsDir, { recursive: true, force: true });
-    }
-});
-
-
